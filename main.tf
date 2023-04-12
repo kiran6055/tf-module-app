9c271ec315d59174c2c0b3047685c661a05a0cc5
kiran1616 <kirankumar7163@gmail.com>
2023-04-12 23:37:40 +0530

    updated script

--- main.tf
+++ main.tf
@@ -21,7 +21,21 @@ resource "aws_security_group" "main" {
 
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-alb-security-group" }
+    { Name = "${var.env}-alb-${var.subnets_name}-security-group" }
   )
 }
 
+# creating application load balancer
+
+resource "aws_lb" "main" {
+  name               = "${var.env}-${var.subnets_name}-alb"
+  internal           = var.internal
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.main.id]
+  subnets            = var.subnet_ids
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-alb-${var.subnets_name}-alb" }
+  )
+}

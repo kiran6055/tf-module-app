0363d89243dc99f9bb25ebad002d895532a1300a
kiran1616 <kirankumar7163@gmail.com>
2023-04-17 15:46:51 +0530

    updated script

--- main.tf
+++ main.tf
@@ -38,6 +38,7 @@ resource "aws_launch_template" "main" {
   name_prefix   = "${var.env}-${var.component}"
   image_id      = data.aws_ami.centos8.id
   instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.main.id]
 }
 
 

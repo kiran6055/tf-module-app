b003415e057bf08b83682a23eb472cc3389d5f77
kiran1616 <kirankumar7163@gmail.com>
2023-04-18 11:43:08 +0530

    updated script

--- user-data.sh
+++ user-data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 labauto
-ansible-pull -i loaclhost, -U https://github.com/ansible-roboshop roboshop.yml -e ROLE_NAMe=${component} -e env=${env} | tee /pot/ansible.log
+ansible-pull -i loaclhost, -U https://github.com/ansible-roboshop roboshop.yml -e ROLE_NAME=${component} -e env=${env} | tee /pot/ansible.log

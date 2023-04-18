84ec683d876b87d8619e313fa3b8408c7815218e
kiran1616 <kirankumar7163@gmail.com>
2023-04-18 17:14:35 +0530

    updated script

--- main.tf
+++ main.tf
@@ -50,6 +50,7 @@ resource "aws_iam_policy" "policy" {
         "Resource" : [
           "arn:aws:ssm:us-east-1:742313604750:parameter/${var.env}.${var.component}*",
           "arn:aws:ssm:us-east-1:742313604750:parameter/nexus*"
+          "arn:aws:ssm:us-east-1:742313604750:parameter/${var.env}.docdb*"
         ]
       },
       {

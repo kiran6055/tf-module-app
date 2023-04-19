990d8a11157b44cc91adfe630385ee8e88637821
kiran1616 <kirankumar7163@gmail.com>
2023-04-19 17:11:47 +0530

    updated script

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_iam_policy" "policy" {
           "arn:aws:ssm:us-east-1:742313604750:parameter/${var.env}.${var.component}*",
           "arn:aws:ssm:us-east-1:742313604750:parameter/nexus*",
           "arn:aws:ssm:us-east-1:742313604750:parameter/${var.env}.docdb*",
-          "arn:aws:ssm:us-east-1:742313604750:parameter/${var.env}.elasticache.Endpoint*"
+          "arn:aws:ssm:us-east-1:742313604750:parameter/${var.env}.elasticache*"
         ]
       },
       {

6ff94e1c96f7468b2a62ceac71ac205cb075c6da
kiran1616 <kirankumar7163@gmail.com>
2023-04-21 12:40:56 +0530

    updated script

--- main.tf
+++ main.tf
@@ -191,7 +191,7 @@ resource "aws_lb_target_group" "target_group" {
 
 
 resource "aws_lb_listener_rule" "backend_rule" {
-  count           = var.app_port != 0 ? 1 : 0
+  count           = var.listener_priority != 0 ? 1 : 0
   listener_arn    = var.listener
   priority        = var.listener_priority
 

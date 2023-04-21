103872ad71f47946763a86dbef2506cc90a28b72
kiran1616 <kirankumar7163@gmail.com>
2023-04-21 14:53:28 +0530

    updated script

--- main.tf
+++ main.tf
@@ -206,3 +206,20 @@ resource "aws_lb_listener_rule" "backend_rule" {
     }
   }
 }
+
+
+
+
+# creating a listener to load balancer for backend
+
+resource "aws_lb_listener" "backend" {
+  count             = var.listener_priority == 0 ? 1 : 0
+  load_balancer_arn = var.alb_arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}

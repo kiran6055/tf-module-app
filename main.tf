382627e893afc06a2d586215d4e1846a84d8203d
kiran1616 <kirankumar7163@gmail.com>
2023-04-24 17:50:58 +0530

    n

--- main.tf
+++ main.tf
@@ -54,7 +54,8 @@ resource "aws_iam_policy" "policy" {
           "arn:aws:ssm:us-east-1:742313604750:parameter/${var.env}.docdb*",
           "arn:aws:ssm:us-east-1:742313604750:parameter/${var.env}.elasticache*",
           "arn:aws:ssm:us-east-1:742313604750:parameter/${var.env}.rds*",
-          "arn:aws:ssm:us-east-1:742313604750:parameter/${var.env}.rabbitmq*"
+          "arn:aws:ssm:us-east-1:742313604750:parameter/${var.env}.rabbitmq*",
+          "arn:aws:ssm:us-east-1:742313604750:parameter/${var.env}.grafana*"
         ]
       },
       {

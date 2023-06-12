98389ae706c80e0805bba23f03dcb78c2628bf33
kiran1616 <kirankumar7163@gmail.com>
2023-06-12 19:07:51 +0530

    m

--- main.tf
+++ main.tf
@@ -97,13 +97,13 @@ resource "aws_security_group" "main" {
     cidr_blocks = var.bastion_cidr
   }
 
-#  ingress {
-#    description = "Prometheus"
-#    from_port   = 9100
-#    to_port     = 9100
-#    protocol    = "tcp"
-#    cidr_blocks = var.monitor_cidr
-#  }
+  ingress {
+    description = "Prometheus"
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = var.monitor_cidr
+  }
 
   egress {
     from_port   = 0
